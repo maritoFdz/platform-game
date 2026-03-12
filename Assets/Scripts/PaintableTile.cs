ce4967a64d57ec73e98b012c9c2439f96da44bcf
maritoFdz <mariofdez314@gmail.com>
2026-03-12 15:49:04 -0500

    changed world from 2d squares to tilemaps

---
+++ Assets/Scripts/PaintableTile.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using static UnityEngine.Tilemaps.Tile;
+
+public enum SurfaceType { Floor, Wall, Slope }
+
+[CreateAssetMenu(menuName = "Tiles/PaintableTile")]
+public class PaintableTile : TileBase
+{
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private SurfaceType surfaceType;
+    public bool isPainted;
+    public SurfaceType GetSurfaceType()
+    {
+        return surfaceType;
+    }
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        tileData.sprite = sprite;
+        tileData.colliderType = ColliderType.Sprite;
+    }
+}
