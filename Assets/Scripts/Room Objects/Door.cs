495c80cc79471072eb83486a965624c440a4a351
maritoFdz <mariofdez314@gmail.com>
2026-04-18 22:47:18 -0500

    doors can close in udrl directions

--- Assets/Scripts/Room Objects/Door.cs
+++ Assets/Scripts/Room Objects/Door.cs
@@ -17,12 +17,41 @@ public class Door : MonoBehaviour
     public bool locked;
     private Vector2 target;
     private Vector2 initialPos;
+    private float spacing;
     private Vector2 dir;
-private Vector2 right;
+    private Vector2 rayOriginVector;
 
     private void Awake()
     {
         initialPos = transform.position;
+    }
+
+    private void Start()
+    {
+        float rotation = transform.eulerAngles.z;
+        switch (rotation)
+        {
+            case 0f:
+                dir = Vector3.down;
+                rayOriginVector = Vector2.right;
+                spacing = col.bounds.size.x / (rayAmount - 1);
+                break;
+            case 90f:
+                dir = Vector3.right;
+                rayOriginVector = Vector2.up;
+                spacing = col.bounds.size.y / (rayAmount - 1);
+                break;
+            case 270f:
+                dir = Vector3.left;
+                rayOriginVector = Vector2.up;
+                spacing = col.bounds.size.y / (rayAmount - 1);
+                break;
+            case 180f:
+                dir = Vector3.up;
+                rayOriginVector = Vector2.right;
+                spacing = col.bounds.size.x / (rayAmount - 1);
+                break;
+        }
     }
 
     private void Update()
@@ -31,22 +60,31 @@ private Vector2 right;
 
         if (isClosing)
         {
-            float moveY = speed * Time.deltaTime;
-            Vector2 origin = new(col.bounds.min.x, col.bounds.min.y - raycastOffset);
-            float minDistance = moveY;
-            float spacing = col.bounds.size.x / (rayAmount - 1);
+            float displacement = speed * Time.deltaTime;
+            float minDistance = displacement;
+
+            Vector2 baseOrigin = Vector2.zero;
+            if (dir == Vector2.down)
+                baseOrigin = new Vector2(col.bounds.min.x, col.bounds.min.y - raycastOffset);
+            else if (dir == Vector2.up)
+                baseOrigin = new Vector2(col.bounds.min.x, col.bounds.max.y + raycastOffset);
+            else if (dir == Vector2.right)
+                baseOrigin = new Vector2(col.bounds.max.x + raycastOffset, col.bounds.min.y);
+            else if (dir == Vector2.left)
+                baseOrigin = new Vector2(col.bounds.min.x - raycastOffset, col.bounds.min.y);
+
             for (int i = 0; i < rayAmount; i++)
             {
-                Vector2 rayOrigin = origin + i * spacing * Vector2.right;
-                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, moveY, collisionLayer);
-                Debug.DrawRay(rayOrigin, Vector2.down * moveY, Color.red);
+                Vector2 rayOrigin = baseOrigin + i * spacing * rayOriginVector;
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, dir, displacement, collisionLayer);
+                Debug.DrawRay(rayOrigin, dir * displacement, Color.red);
                 if (hit)
                     minDistance = Mathf.Min(hit.distance, minDistance);
             }
 
-            moveY = minDistance;
+            displacement = minDistance;
 
-            transform.position += Vector3.down * moveY;
+            transform.position += (Vector3) dir * displacement;
 
             if (Vector3.Distance(transform.position, target) < collisionTolerance)
             {
@@ -77,9 +115,25 @@ private Vector2 right;
     {
         isClosing = true;
         locked = false;
-        Vector2 origin = new(transform.position.x, transform.position.y - col.bounds.size.y / 2 - raycastOffset);
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, Mathf.Infinity, collisionLayer);
+        Vector2 origin = Vector2.zero;
+
+        if (dir == Vector2.down)
+            origin = new Vector2(col.bounds.center.x, col.bounds.min.y - raycastOffset);
+        else if (dir == Vector2.up)
+            origin = new Vector2(col.bounds.center.x, col.bounds.max.y + raycastOffset);
+        else if (dir == Vector2.right)
+            origin = new Vector2(col.bounds.max.x + raycastOffset, col.bounds.center.y);
+        else if (dir == Vector2.left)
+            origin = new Vector2(col.bounds.min.x - raycastOffset, col.bounds.center.y);
+        RaycastHit2D hit = Physics2D.Raycast(origin, dir, Mathf.Infinity, collisionLayer);
+
         if (hit)
-            target = new(transform.position.x, hit.point.y + col.bounds.size.y / 2f);
+        {
+            float extent = (dir == Vector2.up || dir == Vector2.down)
+                ? col.bounds.extents.y
+                : col.bounds.extents.x;
+
+            target = hit.point - dir * extent;
+        }
     }
 }
