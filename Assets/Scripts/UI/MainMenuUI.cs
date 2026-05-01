24fd053214b5e64923e819c364aa0fa2bcaf1983
maritoFdz <mariofdez314@gmail.com>
2026-05-01 14:02:50 -0500

    added main menu behaviour

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
