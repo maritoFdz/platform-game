ccddfd9c10c5ca4be258f656a82cb600d47d7e5a
maritoFdz <mariofdez314@gmail.com>
2026-08-28 19:59:19 -0500

    added 7 colors swap shader and definicion de los scriptable de las palletes

---
+++ Assets/Scripts/Scriptable Objects/ColorPallete.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Color Pallete", menuName = "Scriptable Objects/Color Pallete")]
+public class ColorPallete : ScriptableObject
+{
+    [SerializeField] private Color color1;
+    [SerializeField] private Color color2;
+    [SerializeField] private Color color3;
+    [SerializeField] private Color color4;
+    [SerializeField] private Color color5;
+    [SerializeField] private Color color6;
+    [SerializeField] private Color color7;
+}
